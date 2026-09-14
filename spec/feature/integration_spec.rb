b0503a2a3b0aa50db7393148124d74f5a2c033bc
Rohak <rohakb2004@gmail.com>
2026-09-14 18:22:13 -0500

    Attribute tests w/o implementation completed

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,18 +4,59 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  before do # Set attributes
     visit new_book_path
     fill_in 'book[title]', with: 'harry potter'
+    fill_in 'book[author]', with: 'J. K. Rowling'
+    fill_in 'book[price]', with: 10
+    fill_in 'book[published_date]', with: Date.new(2004, 10, 19)
+  end
+
+  scenario 'valid title' do
     click_on 'Create Book'
     expect(page).to have_content('harry potter')
     expect(page).to have_content('Book was successfully created.')
   end
 
-  scenario 'invalid inputs' do
-    visit new_book_path
+  scenario 'invalid title' do
     fill_in 'book[title]', with: nil
     click_on 'Create Book'
     expect(page).to have_content('Title can\'t be blank')
+  end
+
+  scenario 'valid author' do
+    click_on 'Create Book'
+    expect(page).to have_content('J. K. Rowling')
+    expect(page).to have_content('Book was successfully created.')
+  end
+
+  scenario 'invalid author' do
+    fill_in 'book[author]', with: nil
+    click_on 'Create Book'
+    expect(page).to have_content('Author can\'t be blank')
+  end
+
+  scenario 'valid price' do
+    click_on 'Create Book'
+    expect(page).to have_content('10')
+    expect(page).to have_content('Book was successfully created.')
+  end
+
+  scenario 'invalid price' do
+    fill_in 'book[price]', with: nil
+    click_on 'Create Book'
+    expect(page).to have_content('Price can\'t be blank')
+  end
+
+  scenario 'valid published date' do
+    click_on 'Create Book'
+    expect(page).to have_content(Date.new(2004, 10, 19).to_s)
+    expect(page).to have_content('Book was successfully created.')
+  end
+
+  scenario 'invalid published date' do
+    fill_in 'book[published_date]', with: nil
+    click_on 'Create Book'
+    expect(page).to have_content('Published Date can\'t be blank')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -6,15 +6,43 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'J. K. Rowling', price: 10, published_date: Date.new(2004, 10, 19))
   end
 
-  it 'is valid with valid title' do
-    expect(subject).to be_valid
+  it 'has a valid title' do
+    expect(subject.title).to eq('harry potter')
   end
 
-  it 'is not valid without a title' do
+  it 'has an invalid title' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  
+  it 'has a valid author' do
+    expect(subject.author).to eq('J. K. Rowling')
+  end
+
+  it 'has an invalid author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'has a valid price' do
+    expect(subject.price).to eq(10)
+  end
+
+  it 'has an invalid price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'has a valid published date' do
+    expect(subject.published_date).to eq(Date.new(2004, 10, 19))
+  end
+
+  it 'has an invalid published date' do
+    subject.published_date = nil
     expect(subject).not_to be_valid
   end
 end
